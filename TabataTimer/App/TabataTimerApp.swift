54a28d53a8634b9b1c0029093b5abd89e5de472f
Ivan Revchuk <ivanrevchukpython@gmail.com>
2025-11-24 12:53:33 +0200

    chore: initial Xcode project

---
+++ TabataTimer/App/TabataTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataTimerApp.swift
+//  TabataTimer
+//
+//  Created by Ivan Revchuk on 22.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TabataTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
